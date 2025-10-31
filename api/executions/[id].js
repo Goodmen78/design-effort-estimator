71d9e0b1c8ac8250b03ab0396c68f703653ca9a0
Goodmen78 <mugiwarachan78@gmail.com>
2025-10-31 14:20:50 +0530

    test

--- api/executions/[id].js
+++ api/executions/[id].js
@@ -30,7 +30,7 @@ export default async function handler(req, res) {
       }
     );
 
-    const data = await n8nResponse.json();
+    const data = await n8nResponse;
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({
