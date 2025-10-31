27e5b971148a0f9098ac7bce879b01330638ea2f
Goodmen78 <mugiwarachan78@gmail.com>
2025-10-31 13:49:32 +0530

    testing

---
+++ api/executions/[id].js
@@ -0,0 +1,20 @@
+export default function handler(req, res) {
+  const {
+    query: { id },
+    method,
+  } = req;
+
+  switch (method) {
+    case "GET":
+      res.status(200).json({ message: `GET request received`, id });
+      break;
+
+    case "POST":
+      res.status(200).json({ message: `POST request received`, body: req.body, id });
+      break;
+
+    default:
+      res.setHeader("Allow", ["GET", "POST"]);
+      res.status(405).json({ error: `Method ${method} not allowed` });
+  }
+}
